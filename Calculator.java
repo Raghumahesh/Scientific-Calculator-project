38417d0a7bd71e65327d2ee73b3842d03b3d6d6f
Raghu Medarametla <60712952+Raghumahesh@users.noreply.github.com>
2023-11-28 17:27:58 -0600

    Calculator.java
    I started the code by creating the awt, swing variables that are required. Initialized constructor .

--- Calculator.java
+++ Calculator.java
@@ -1,1 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+public class Calculator extends JFrame {
+
+    private JTextField display;
+    private StringBuilder currentInput;
+    private String lastOperation;
+    private double result;
+
+    public Calculator() {
+        setTitle("Calculator");
+        setSize(300, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        currentInput = new StringBuilder();
+        lastOperation = "";
+        result = 0;
+
+        setupUI();
+    }
+}
