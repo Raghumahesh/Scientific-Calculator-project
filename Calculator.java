2e36249769ab5d89c486313174ac04f03c8c27c8
Raghu Medarametla <60712952+Raghumahesh@users.noreply.github.com>
2023-11-28 19:10:24 -0600

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -21,4 +21,26 @@ public class Calculator extends JFrame {
 
         setupUI();
     }
+    private class ButtonClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            JButton source = (JButton) e.getSource();
+            String buttonText = source.getText();
+
+            if (buttonText.matches("[0-9]")) {
+                handleDigitClick(buttonText);
+            } else if (buttonText.equals(".")) {
+                handleDecimalClick();
+            } else if (buttonText.equals("=")) {
+                handleEqualClick();
+            } else if (buttonText.equals("AC")) {
+                handleAllClearClick();
+            } else if (buttonText.equals("+/-")) {
+                handleChangeSignClick();
+            } else if (buttonText.equals("C")) {
+                handleClearClick();
+            } else {
+                handleOperationClick(buttonText);
+            }
+        }
+    }
 }
